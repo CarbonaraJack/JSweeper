ef31f0dd0ee6f016fe6df60e8b580d93edd0c3c7
CarbonaraJack <linked.uno@gmail.com>
2018-02-22 16:35:26 +0100

    Routing server and index.html implemented

---
+++ docs/minesweeper.js
@@ -0,0 +1,10 @@
+
+$('#canvas').ready(function(){
+  var canvas = $('#canvas').get(0);
+  //set canvas size. To do: adapt canvas size to the window size
+  canvas.width = 630;
+  canvas.height = 630;
+  //get drawable canvas context
+  var ctx = canvas.getContext('2d');
+  /**GAME CODE**/
+});
