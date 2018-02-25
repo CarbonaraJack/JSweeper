eb25a50e8c86f9043f7a4e1fa9aa0b2ce1728a74
CarbonaraJack <linked.uno@gmail.com>
2018-02-25 19:09:57 +0100

    Code refactoring and Cell prototype edit I have to store the coordinates of each single cell in order to catch onClick and onHover events, since the canvas element is a single html element. I also created the Cell.draw function in order to clean up code and the Cell.assignPos and more importantly the Field.assignConst in order to assign to the object itself the values that I need to calculate only once to avoid repeating calculations every time I have to redraw the field.

--- docs/minesweeper.js
+++ docs/minesweeper.js
@@ -11,14 +11,124 @@ function getRandomInt(min, max) {
   Cell object
 **/
 function Cell(type,value){
+  Cell.prototype.assignPos = assignCellPos;
+  Cell.prototype.draw = drawCell;
   this.type = type; //can be flag, shown or hidden
   this.value = value; // B = bomb, otherwise we have proximity number
+}
+/**
+  Function that sets the absolute coordinates of a cell
+**/
+var assignCellPos = function(topX,topY,cellSize){
+  this.topX = topX;
+  this.topY = topY;
+  this.cellSize = cellSize;
+  this.botX = topX+cellSize-1;
+  this.botY = topY+cellSize-1;
+}
+/**
+  Function that draws a single cell
+**/
+var drawCell = function(ctx){
+  if(this.type==="hidden"){
+    //draw the backround of the cell
+    ctx.beginPath();
+    ctx.rect(this.topX,this.topY,this.cellSize,this.cellSize);
+    ctx.fillStyle = "#d3d3d3";
+    ctx.fill();
+    //draw the border
+    //I have to simulate standard HTML solid border behaviour
+    //this is a pain, but I want to stick with the original minesweeper look
+    //and feel as much as I can
+
+    //Top and left borders
+    ctx.beginPath();
+    ctx.moveTo(this.topX,this.topY);
+    ctx.lineTo(this.botX,this.topY);
+    ctx.lineTo(this.botX-1,this.topY+1);
+    ctx.lineTo(this.topX+1,this.topY+1);
+    ctx.lineTo(this.topX+1,this.botY-1);
+    ctx.lineTo(this.topX,this.botY);
+    ctx.closePath();
+    ctx.strokeStyle = "#f5f5f5";
+    ctx.stroke();
+    //bottom and right borders
+    ctx.beginPath();
+    ctx.moveTo(this.botX,this.topY+1);
+    ctx.lineTo(this.botX,this.botY);
+    ctx.lineTo(this.topX+1,this.botY);
+    ctx.lineTo(this.topX+2,this.botY-1);
+    ctx.lineTo(this.botX-1,this.botY-1);
+    ctx.lineTo(this.botX-1,this.topY+1);
+    ctx.closePath();
+    ctx.strokeStyle = "#808080";
+    ctx.stroke();
+  }else if(this.type==="shown"){
+    //draw the backround of the cell
+    ctx.beginPath();
+    ctx.rect(this.topX,this.topY,this.cellSize,this.cellSize);
+    ctx.fillStyle = "#d3d3d3";
+    ctx.fill();
+    //draw the border
+    ctx.beginPath();
+    ctx.rect(this.topX,this.topY,this.cellSize,this.cellSize);
+    ctx.strokeStyle = "#696969";
+    ctx.stroke();
+    //get the text of the cell
+    var cellText = this.value;
+    if(cellText === "B"){
+      //For now if I have a bomb I will draw *, will change it with a mine
+      //picture in the future.
+      cellText = "*";
+    }
+    //Draw the text only if I have to
+    if(cellText!==0){
+      //the font size will be 80% of the cell size
+      ctx.font = this.cellSize*0.8 + 'px sans-serif';
+      ctx.textAlign = "center";
+      ctx.textBaseline = "bottom";
+      //choose a different color for each number
+      switch (cellText) {
+        case 1:
+          ctx.fillStyle = "#0000ff";
+          break;
+        case 2:
+          ctx.fillStyle = "#006400";
+          break;
+        case 3:
+          ctx.fillStyle = "#ff0000";
+          break;
+        case 4:
+          ctx.fillStyle = "#ff00ff";
+          break;
+        case 5:
+          ctx.fillStyle = "#00ffff";
+          break;
+        case 6:
+          ctx.fillStyle = "#90ee90";
+          break;
+        case 7:
+          ctx.fillStyle = "#ffa500";
+          break;
+        case 8:
+          ctx.fillStyle = "#000000";
+          break;
+        default:
+          ctx.fillStyle = "#000000";
+      }
+      ctx.fillText(cellText,
+        //align the text at the bottm middle of the cell. This together with the
+        //80% font size magically centers the text. TEST WITH MULTIPLE BROWSERS!
+        this.topX+this.cellSize/2,this.topY+this.cellSize);
+    }
+  }
 }
 /**
   Field object
 **/
 function Field(columns,rows,mines){
   Field.prototype.draw = drawField;
+  Field.prototype.assignConst = assignFieldConst;
   this.columns = columns;
   this.rows = rows;
   //generate the array
@@ -97,10 +207,13 @@ function Field(columns,rows,mines){
     }
   }
 }
+
 /**
-  Function that draws the field of play
+  Function that sets the constant variables of a field and sets the position of
+  each individual cell
 **/
-var drawField = function (ctx, margin, width, height){
+var assignFieldConst = function(margin, width, height){
+  this.margin = margin;
   var cellHeight=height/this.rows;
   var cellWidth=width/this.columns;
   //I want squared cells, not rectangles so I keep the shortest edge
@@ -109,106 +222,20 @@ var drawField = function (ctx, margin, width, height){
     cellSize=cellWidth;
   }
   //cellSize=25;//default minesweeper dimensions
-  /*
-  ctx.beginPath();
-  ctx.rect(margin,margin,width,height);
-  ctx.strokeStyle = "000";
-  ctx.stroke();
-  */
+  //now that I have my cellSize I can assign the position to each cell
   for (var i = 0; i < this.columns; i++) {
     for (var j = 0; j < this.rows; j++) {
-      if(this.cells[i][j].type==="hidden"){
-        //draw the backround of the cell
-        ctx.beginPath();
-        ctx.rect(margin+(i*cellSize),margin+(j*cellSize),cellSize,cellSize);
-        ctx.fillStyle = "#d3d3d3";
-        ctx.fill();
-        //draw the border
-        //I have to simulate standard HTML solid border behaviour
-        //this is a pain, but I want to stick with the original minesweeper look
-        //and feel as much as I can
-
-        //Top and left borders
-        ctx.beginPath();
-        ctx.moveTo(margin+(i*cellSize),margin+(j*cellSize));
-        ctx.lineTo(margin+(i*cellSize)+cellSize,margin+(j*cellSize));
-        ctx.lineTo(margin+(i*cellSize)+cellSize-1,margin+(j*cellSize)+1);
-        ctx.lineTo(margin+(i*cellSize)+1,margin+(j*cellSize)+1);
-        ctx.lineTo(margin+(i*cellSize)+1,margin+(j*cellSize)+cellSize-2);
-        ctx.lineTo(margin+(i*cellSize),margin+(j*cellSize)+cellSize-1);
-        ctx.closePath();
-        ctx.strokeStyle = "#f5f5f5";
-        ctx.stroke();
-        //bottom and right borders
-        ctx.beginPath();
-        ctx.moveTo(margin+(i*cellSize)-1+cellSize,margin+(j*cellSize));
-        ctx.lineTo(margin+(i*cellSize)-1+cellSize,margin+(j*cellSize)+cellSize-1);
-        ctx.lineTo(margin+(i*cellSize),margin+(j*cellSize)+cellSize-1);
-        ctx.lineTo(margin+(i*cellSize)+1,margin+(j*cellSize)+cellSize-2);
-        ctx.lineTo(margin+(i*cellSize)-1+cellSize-1,margin+(j*cellSize)+cellSize-2);
-        ctx.lineTo(margin+(i*cellSize)-1+cellSize-1,margin+(j*cellSize)+1);
-        ctx.closePath();
-        ctx.strokeStyle = "#808080";
-        ctx.stroke();
-      }else if(this.cells[i][j].type==="shown"){
-        //draw the backround of the cell
-        ctx.beginPath();
-        ctx.rect(margin+(i*cellSize),margin+(j*cellSize),cellSize,cellSize);
-        ctx.fillStyle = "#d3d3d3";
-        ctx.fill();
-        //draw the border
-        ctx.beginPath();
-        ctx.rect(margin+(i*cellSize),margin+(j*cellSize),cellSize,cellSize);
-        ctx.strokeStyle = "#696969";
-        ctx.stroke();
-        //get the text of the cell
-        var cellText = this.cells[i][j].value;
-        if(cellText === "B"){
-          //For now if I have a bomb I will draw *, will change it with a mine
-          //picture in the future.
-          cellText = "*";
-        }
-        //Draw the text only if I have to
-        if(cellText!==0){
-          //the font size will be 80% of the cell size
-          ctx.font = cellSize*0.8 + 'px sans-serif';
-          ctx.textAlign = "center";
-          ctx.textBaseline = "bottom";
-          //choose a different color for each number
-          switch (cellText) {
-            case 1:
-              ctx.fillStyle = "#0000ff";
-              break;
-            case 2:
-              ctx.fillStyle = "#006400";
-              break;
-            case 3:
-              ctx.fillStyle = "#ff0000";
-              break;
-            case 4:
-              ctx.fillStyle = "#ff00ff";
-              break;
-            case 5:
-              ctx.fillStyle = "#00ffff";
-              break;
-            case 6:
-              ctx.fillStyle = "#90ee90";
-              break;
-            case 7:
-              ctx.fillStyle = "#ffa500";
-              break;
-            case 8:
-              ctx.fillStyle = "#000000";
-              break;
-            default:
-              ctx.fillStyle = "#000000";
-          }
-          ctx.fillText(cellText,
-            //align the text at the bottm middle of the cell. This together with the
-            //80% font size magically centers the text. TEST WITH MULTIPLE BROWSERS!
-            margin+(i*cellSize)+cellSize/2,margin+(j*cellSize)+cellSize);
-        }
-      }
+      this.cells[i][j].assignPos(margin+cellSize*i,margin+cellSize*j,cellSize);
+    }
+  }
+}
+/**
+  Function that draws the field of play
+**/
+var drawField = function (ctx){
+  for (var i = 0; i < this.columns; i++) {
+    for (var j = 0; j < this.rows; j++) {
+      this.cells[i][j].draw(ctx);
     }
   }
 }
@@ -225,5 +252,6 @@ $('#canvas').ready(function(){
   var margin = 15;
   var width = canvas.width - 2*margin;
   var height = canvas.height - 2*margin;
-  field.draw(ctx,margin,width,height);
+  field.assignConst(margin,width,height);
+  field.draw(ctx);
 });
